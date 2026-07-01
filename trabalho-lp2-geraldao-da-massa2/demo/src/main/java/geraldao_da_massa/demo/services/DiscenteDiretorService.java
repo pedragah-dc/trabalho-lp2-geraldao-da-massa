31592bb67870efa18100453fd9febd1e07cee52b
Diego Oliveira Teixeira <diego.ot@discente.ufma.br>
2026-06-30 23:46:54 -0300

    fixing bugs

--- trabalho-lp2-geraldao-da-massa2/demo/src/main/java/geraldao_da_massa/demo/services/DiscenteDiretorService.java
+++ trabalho-lp2-geraldao-da-massa2/demo/src/main/java/geraldao_da_massa/demo/services/DiscenteDiretorService.java
@@ -1,27 +1,13 @@
 package geraldao_da_massa.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DiscenteDiretorService {
 
-    private final OportunidadesService oportunidadeService;
+    @Autowired
+    private OportunidadesService oportunidadeService;
 
-    public DiscenteDiretorService(OportunidadesService oportunidadeService) {
-        this.oportunidadeService = oportunidadeService;
-    }
-
-    // DiscenteDiretor cria e já submete para aprovação em um passo
-//    public Oportunidade criarESubmeter(String titulo, String descricao,
-//                                       TiposOportunidade tipo, TiposModalidade modalidade,
-//                                       Integer cargaHoraria, Integer vagas,
-//                                       LocalDateTime inicio, LocalDateTime fim,
-//                                       LocalDateTime dataInicioInscricoes, LocalDateTime dataFimInscricoes,
-//                                       DiscenteDiretor autor, Docente docenteResponsavel) {
-//
-//        Oportunidade op = oportunidadeService.criarOportunidade(
-//                titulo, descricao, tipo, modalidade, cargaHoraria, vagas,
-//                inicio, fim, dataInicioInscricoes, dataFimInscricoes,
-//                autor, docenteResponsavel);
-//
-//        oportunidadeService.submeterParaAprovacao(op);
-//        return op;
-//    }
+    // TODO: implementar criarESubmeter quando o fluxo de DiscenteDiretor for definido
 }
--- trabalho-lp2-geraldao-da-massa2/demo/src/main/java/geraldao_da_massa/demo/services/NotificacaoService.java
+++ trabalho-lp2-geraldao-da-massa2/demo/src/main/java/geraldao_da_massa/demo/services/NotificacaoService.java
@@ -1,5 +1,8 @@
 package geraldao_da_massa.demo.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificacaoService {
-//??
+    // TODO: implementar notificações
 }
